a5ae6eab3cf3d2f4ec25ce9051f60e4c8dbccaae
DanteCesca <dantecesca597@gmail.com>
2024-02-28 19:04:02 -0300

    feat(habitacion.js): se modifico el lenguaje de los nombres de las propiedades a ingles

--- models/habitacion.js
+++ models/habitacion.js
@@ -1,13 +1,15 @@
 const { Schema, model } = requiered('mongoose');
 
 const HabitacionSchema = Schema({
-    numero: {type: String, requiered:[true, 'El numero es obligatorio'],unique: true},
-    camas: {type: Number, requiered:[true, 'El numero de camas es obligatorio']},
-    baño: {type: Boolean, default: false,},
-    img: {type: String},
-    oferta: {type: Boolean, default: false},
-    precio: {type: Number, requiered:[true,'El precio es necesario']},
-    reservado: {type: Boolean, default: false},
+    number: {type: String, requiered:[true, 'El numero es obligatorio'],unique: true},
+    beds: {type: Number, requiered:[true, 'El numero de camas es obligatorio']},
+    bathroom: {type: Boolean, default: false,},
+    img: {type: String, requiered:[true, 'Es necesario poner la ruta de imagen']},
+    img2: {type: String, requiered:[true, 'Es necesario poner la ruta de imagen']},
+    img3: {type: String, requiered:[true, 'Es necesario poner la ruta de imagen']},
+    sale: {type: Boolean, default: false},
+    price: {type: Number, requiered:[true,'El precio es necesario']},
+    reserved: {type: Boolean, default: false},
 });
 
 module.exports = model('Habitacion', HabitacionSchema)
