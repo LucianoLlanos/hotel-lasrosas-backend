653ebb76252a60c1981de55163a3d0c71366c6c6
tunombre <email>
2024-03-30 18:46:51 -0300

    Se creó controladores de consulta

---
+++ controllers/consulta.js
@@ -0,0 +1,56 @@
+const {response, request} = require('express')
+const Consulta = require('../models/consulta') 
+
+const consultasGet = async(req=request, res=response) => {
+    const datos = req.query;
+    
+    const [ total, consultas ] = await Promise.all([Consulta.countDocuments(query), Consulta.find(query)]);
+
+    res.json({
+        mensaje: "Consultas obtenidas",
+        total,
+        consultas
+    })
+}
+
+const consultaGet = async(req=request, res=response) => {
+    const {id} = req.params;
+
+    const consulta = await Consulta.findById(id);
+
+    res.json({
+        mensaje: 'Consulta obtenida',
+        consulta,
+    })
+};
+
+const consultaPost = async(req=request, res=response) => {
+    const datos = req.body;
+    const {mail, consultas, telefono} = datos;
+    const consulta = new Consulta({mail, consulta, telefono});
+
+
+    await consulta.save();
+    res.json({
+        mensaje: "Consulta enviada",
+        consulta
+         
+    })
+}
+
+const consultaDelete = async(req=request, res=response) => {
+    const {id} = reqparams;
+
+    const ConsultaDeleted = await Consulta.findByIdAndDelete(id);
+
+    res.jason({
+        mensaje: "Consulta elimindada"
+    })
+}
+
+module.exports = {
+    ConsultasGet,
+    ConsultaGet,
+    ConsultaPost,
+    ConsultaDelete,
+}
--- models/consulta.js
+++ models/consulta.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-const ContultaSchema = Schema ({
+const ConsultaSchema = Schema ({
     mail:  {type: String, required:[true, 'el correo es obligatorio'], unique: true},
     consulta:{type: String, required:[true, 'la consulta es obligatoria']},
     telefono:{type: Number}
